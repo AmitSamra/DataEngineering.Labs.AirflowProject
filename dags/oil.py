6cd3ae45805e9c8d6d89fb67b2a894b08f7750de
AmitSamra <Amit.Samra@outlook.com>
2020-04-03 18:13:45 -0400

    Wrote test fcn

--- dags/oil.py
+++ dags/oil.py
@@ -1,20 +1,41 @@
+import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import timedelta, datetime
+import csv
+import requests
+
 
 default_args = {
 	'owner':'Mr. Amit',
 	'start_date': datetime.now(),
-	'retries':2,
+	'retries':5,
 	'retry_delay':timedelta(minutes=1)
 }
 
 dag = DAG(
-	'ex1',
+	'oil',
 	default_args = default_args,
 	description = 'Oily Money',
 	#schedule_interval=timedelta(hours=1)
 	)
 
-def read_csv():
+
+def get_unrate():
+	url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=968&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=UNRATE&scale=left&cosd=1948-01-01&coed=2020-03-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2009-06-01&line_index=1&transformation=lin&vintage_date=2020-04-03&revision_date=2020-04-03&nd=1948-01-01'
+	response = requests.get(url)
+
+	with open('UNRATE.csv', 'w') as f:
+		writer = csv.writer(f)
+		for line in response.iter_lines():
+			writer.writerow(line.decode('utf-8').split(','))
+
+
+t1 = PythonOperator(
+	task_id = 'dl_unemp_rate_us',
+	python_callable = get_unrate,
+	provide_context = False,
+	dag = dag,
+	)
+
