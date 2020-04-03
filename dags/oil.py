db1e2945c1d147fc87fde23e749d172deb989820
AmitSamra <Amit.Samra@outlook.com>
2020-04-03 19:24:51 -0400

    DAG success trial fcn success

--- dags/oil.py
+++ dags/oil.py
@@ -38,3 +38,38 @@ t1 = PythonOperator(
 	dag = dag,
 	)
 
+
+'''def get_txur():
+	url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=968&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=TXUR&scale=left&cosd=1976-01-01&coed=2020-02-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2009-06-01&line_index=1&transformation=lin&vintage_date=2020-04-03&revision_date=2020-04-03&nd=1976-01-01'
+	response = requests.get(url)
+
+	with open('TXUR.csv', 'wb') as f:
+		f.write(response.content)
+
+t2 = PythonOperator(
+	task_id = 'get_txur',
+	python_callable = get_txur,
+	provide_context = False,
+	dag = dag,
+	)
+
+
+
+
+
+t1 > [t2]'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
