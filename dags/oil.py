038ce67087fc25eb2f109ddcbf9566526e6bdd30
AmitSamra <Amit.Samra@outlook.com>
2020-04-04 02:30:40 -0400

    Began json request

--- dags/oil.py
+++ dags/oil.py
@@ -299,6 +299,10 @@ t15 = PythonOperator(
 	)
 
 
+def read_json(file_path):
+	file_path = 'http://api.eia.gov/series/?api_key=f808545279092f7505cb23185ce4123e&series_id=PET.MCRFPUS2.M'
+	with open(file_path, 'r')
+
 t1 >> [t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15]
 
 
