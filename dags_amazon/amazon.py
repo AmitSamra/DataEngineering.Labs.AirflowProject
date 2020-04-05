68d4ced21013bacdc3b76e8c10d99f321e9b58ad
AmitSamra <Amit.Samra@outlook.com>
2020-04-05 03:46:20 -0400

    Fixed break in import to postgres task

--- dags_amazon/amazon.py
+++ dags_amazon/amazon.py
@@ -7,11 +7,13 @@ import csv
 import requests
 import os
 from airflow.operators.postgres_operator import PostgresOperator
+import numpy as np
+import pandas as pd
 
 
 default_args = {
 	'owner':'Mr. Amit',
-	'start_date': datetime.now(),
+	'start_date': datetime(2020,4,5,0),
 	'retries':0,
 	'retry_delay':timedelta(minutes=1)
 }
@@ -21,7 +23,8 @@ dag = DAG(
 	'amazon',
 	default_args = default_args,
 	description = 'amazon',
-	#schedule_interval=timedelta(hours=1)
+	schedule_interval = timedelta(hours=1),
+	catchup = False,
 	)
 
 
@@ -42,11 +45,44 @@ t1 = PythonOperator(
 	dag = dag,
 	)
 
+def etl_csv():
+	"""
+	Cleans CSV to eliminate NaN and drop certain rows where item_total = 0
+	"""
+	df = pd.read_csv('amazon_purchases.csv')
+	df.columns = ['order_id', 'order_date', 'category', 'website', 'condition', 'seller', 'list_price_per_unit', 'purchase_price_per_unit', 'quantity', 'shipment_date', 'carrier_name', 'item_subtotal', 'item_subtotal_tax','item_total']
+	df.category.fillna('', inplace = True)
+	df.condition.fillna('', inplace = True)
+	df.carrier_name.fillna('', inplace = True)
+	df.shipment_date.fillna('', inplace = True)
+	df = df[df.list_price_per_unit != 0]
+	df = df[df.purchase_price_per_unit != 0]
+	df = df[df.item_total != 0]
+	df['list_price_per_unit'] = df['list_price_per_unit'].str.replace('$','').str.replace(',','')
+	df['purchase_price_per_unit'] = df['purchase_price_per_unit'].str.replace('$','').str.replace(',','')
+	df['item_subtotal'] = df['item_subtotal'].str.replace('$','').str.replace(',','')
+	df['item_subtotal_tax'] = df['item_subtotal_tax'].str.replace('$','').str.replace(',','')
+	df['item_total'] = df['item_total'].str.replace('$','').str.replace(',','')
+	df['list_price_per_unit'] = df['list_price_per_unit'].astype(float)
+	df['purchase_price_per_unit'] = df['purchase_price_per_unit'].astype(float)
+	df['item_subtotal'] = df['item_subtotal'].astype(float)
+	df['item_subtotal_tax'] = df['item_subtotal_tax'].astype(float)
+	df['item_total'] = df['item_total'].astype(float)
+	df.to_csv('amazon_purchases_2.csv', index=False)
 
-t2 = PostgresOperator(
+
+t2 = PythonOperator(
+	task_id = 'etl_csv',
+	python_callable = etl_csv,
+	provide_context = False,
+	dag = dag,
+	)
+
+
+t3 = PostgresOperator(
 	task_id = 'create_table',
 	postgres_conn_id = 'postgres_amazon',
-	sql = '''CREATE TABLE amazon.amazon_purchases(
+	sql = '''CREATE TABLE IF NOT EXISTS amazon.amazon_purchases(
 		order_id integer primary key, 
 		order_date date,
 		category varchar(255),
@@ -65,12 +101,14 @@ t2 = PostgresOperator(
 	)
 
 
-t3 = PostgresOperator(
+path = os.path.join(os.path.dirname(__file__),'../amazon_purchases_2.csv')
+
+t4 = PostgresOperator(
 	task_id = 'import_to_postgres',
 	postgres_conn_id = 'postgres_amazon',
-	sql = "COPY amazon.amazon_purchases FROM '/Users/asamra/dev/DataEngineering.Labs.AirflowProject/data_amazon/amazon_purchases.csv' DELIMITER ',' CSV HEADER;",
+	sql = f"DELETE FROM amazon.amazon_purchases; COPY amazon.amazon_purchases FROM '{path}' DELIMITER ',' CSV HEADER;",
 	dag = dag,
 	)
 
 
-t1 >> [t2, t3]
+t1 >> t2 >> t3 >> t4
